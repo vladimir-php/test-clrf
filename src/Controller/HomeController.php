ed68738419e5d2f83fccb3a6cf962d4e15a30431
Vladimir Makarov <vladimir.php@gmail.com>
2024-07-23 08:35:30 +0200

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,14 +11,9 @@
 
 namespace App\Controller;
 
-
-// src/Controller/HelloController.php
-namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
 
 /**
  *
