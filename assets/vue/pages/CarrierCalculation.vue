f746d9f23d58e08fe643bb4013f9b0f138e68825
Vladimir Makarov <vladimir.php@gmail.com>
2024-07-23 11:04:12 +0200

    Minor changes with vue template

--- assets/vue/pages/CarrierCalculation.vue
+++ assets/vue/pages/CarrierCalculation.vue
@@ -5,15 +5,18 @@
     <div class="px-4 py-2 rounded-lg shadow-lg text-center">
 
         <div class="flex space-x-4">
-            <div class="w-full px-4 py-2 text-center">
+            <div class="w-full px-4 py-2">
                 <input-field placeholder="Cargo weight"></input-field>
             </div>
         </div>
+        
 
-        <!--
-        <div class="flex space-x-4">
+        <div class="flex justify-end space-x-4 px-4 py-2">
             <submit-button @submit="submitRequest" :loading="loading" />
         </div>
+
+        <!--
+
 
         <response-output :success="responseSuccess" :output="responseOutput" />
         -->
@@ -25,18 +28,24 @@
 
 import axios from 'axios'
 import InputField from "../components/form/base/InputField.vue";
+import SubmitButton from "../components/form/base/SubmitButton.vue";
 
 export default {
     computed: {
     },
     components: {
-        InputField
+        InputField,
+        SubmitButton
     },
     props: {
     },
     name: "Carrier Calculation",
     data() {
         return {
+            response: null,
+            responseSuccess: false,
+            responseOutput: '',
+            loading: false
         }
     },
     async mounted() {
@@ -49,6 +58,22 @@ export default {
     created() {
     },
     methods: {
+        async submitRequest() {
+            this.loading = true;
+
+            /*
+            let query = new Query();
+            this.response = await query.queryRequest(this.request);
+
+            let outputData = this.response.success() ? this.response.data() : this.response.rawData();
+            this.responseOutput = JSON.stringify(outputData, null, 2);
+            this.responseSuccess = this.response.success();
+
+             */
+            // this.loading = false;
+
+            this.$emit('onResponse', outputData, this.responseSuccess);
+        },
     }
 }
 </script>
